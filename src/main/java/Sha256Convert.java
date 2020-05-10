1952faed5b4e2c03b3951f9467efaa50f0991b39
Naoyuki <t08051926125@gmail.com>
2020-05-10 21:26:33 +0900

    SHA-2を追加

---
+++ src/main/java/Sha256Convert.java
@@ -0,0 +1,25 @@
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import hash.SHA2;
+
+import java.security.MessageDigest;
+
+public class Sha256Convert extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        String selectedText = editor.getSelectionModel().getSelectedText();
+
+        String sb = SHA2.getSHA256(selectedText);
+
+        Notifications.Bus.notify(
+                new Notification("hash converter", "convert sha256", sb, NotificationType.INFORMATION)
+        );
+    }
+}
